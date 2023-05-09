c742f908ccf98132932907b3f743d747c4c57f49
choiwonho <fhfwk9188@gmail.com>
2023-05-10 08:28:07 +0900

    0.0.7 textarea

--- wonho-ui/component/TextArea.jsx
+++ wonho-ui/component/TextArea.jsx
@@ -12,24 +12,10 @@ const TextArea = ({
                 onClick,
                 onFocus,
                 onKeyDown,
-                wrapClassName,
                 className,
-                otlClassName,
                 }) => {
-  let [total, setTotal] = React.useState(defaultValue ? defaultValue.length : 0);
-  const keyUp = (e) => {
-    let number = e.target.value.length;
-    if ( number < maxLength) {
-      setTotal(number);  
-    } else if ( number == maxLength ) {
-      setTotal(number);  
-      alert('최대 글자 수에 도달했습니다.')
-    }
-  }
-  
   return (
     <>
-    <div className={styles['textarea-wrap']}>
       <textarea 
         className={
           styles.textarea
@@ -43,18 +29,11 @@ const TextArea = ({
         rows={rows}
         readOnly={readOnly}
         disabled={disabled} 
-        onChange={keyUp}
+        onChange={onChange}
         onClick={onClick}
         onFocus={onFocus}
         onKeyDown={onKeyDown}
       />
-      {
-        maxLength ?
-        <span className={styles['textarea-max']}> {total} / {maxLength} 글자 </span>
-        :
-        <></>
-      }
-    </div>
     </>
   );
 }
